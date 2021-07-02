41e3ff50414cb10163c4642e06063a3222798284
Fran Dios <frankdiox@gmail.com>
2021-07-02 18:38:55 +0200

    Add waves SVG to Features section

--- src/templates/Home/Features.vue
+++ src/templates/Home/Features.vue
@@ -30,7 +30,7 @@ export default {
 </script>
 
 <template>
-  <div class="py-16 sm:py-8">
+  <div class="relative py-6 my-48 sm:py-8 bg-gray-soft" :class="$style.waves">
     <div class="container mx-auto">
       <h3
         class="max-w-4xl mx-auto text-3xl font-semibold text-center mb-28 text-primary"
@@ -69,3 +69,27 @@ export default {
     </div>
   </div>
 </template>
+
+<style lang="scss" module>
+.waves {
+  &::before {
+    position: absolute;
+    top: -100px;
+    width: 100%;
+    height: 100px;
+    content: '';
+    background-image: url(/img/wave-upper.svg);
+    background-repeat: repeat-x;
+  }
+
+  &::after {
+    position: absolute;
+    bottom: -100px;
+    width: 100%;
+    height: 100px;
+    content: '';
+    background-image: url(/img/wave-lower.svg);
+    background-repeat: repeat-x;
+  }
+}
+</style>
