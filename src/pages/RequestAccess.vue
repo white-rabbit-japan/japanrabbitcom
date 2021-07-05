e7e311305e35d8ac832f46cd1d0969bdbeaf5778
Fran Dios <frankdiox@gmail.com>
2021-07-05 20:24:04 +0200

    Increase iframe height for mobile

--- src/pages/RequestAccess.vue
+++ src/pages/RequestAccess.vue
@@ -1,11 +1,11 @@
 <template>
   <Layout>
-    <div class="container py-20 mx-auto">
+    <div class="container pt-10 mx-auto">
       <div class="max-w-4xl mx-auto">
         <iframe
           src="https://tally.so/embed/wbWJEn?alignLeft=1&transparentBackground=1"
           width="100%"
-          height="1100"
+          height="1400"
           frameborder="0"
           marginheight="0"
           marginwidth="0"
