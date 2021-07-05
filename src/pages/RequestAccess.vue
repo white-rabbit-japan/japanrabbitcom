0eca25f3a8fad1b8dff1ad9085f7f71b101f164b
Fran Dios <frankdiox@gmail.com>
2021-07-05 20:21:33 +0200

    Add RequestAccess page

---
+++ src/pages/RequestAccess.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <div class="container py-20 mx-auto">
+      <div class="max-w-4xl mx-auto">
+        <iframe
+          src="https://tally.so/embed/wbWJEn?alignLeft=1&transparentBackground=1"
+          width="100%"
+          height="1100"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          title="Join the waitlist."
+        ></iframe>
+      </div>
+    </div>
+  </Layout>
+</template>
